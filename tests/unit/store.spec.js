cd21c618bb360337ec9ae393026e8d004ec07760
Tristan Rücker <Tristan.Ruecker@dermalog.com>
2020-05-05 12:39:02 +0200

    Write tests for getters

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -1,84 +1,57 @@
-// mutations.spec.js
-// destructure assign `mutations`
-import { mutations } from '../../src/store';
-import Vuex from 'vuex';
+// store.spec.js
+import moment from "moment";
+import { getters } from "../../src/store";
 
-describe('testing mutations...', () => {
-  it('should RECALCULATE_HOURS', () => {
-    let insertedEntry = {
-      hours: 4
+describe("test store getters", () => {
+  it("should getOverallHours", () => {
+    // init
+    const state = {
+      insertedEntries: [
+        {
+          hours: 4
+        },
+        { hours: 6 },
+        { hours: 7.5 }
+      ]
     };
 
-    let insertedEntry2 = {
-      hours: 4.25
-    };
+    // call method
+    let overallHours = getters.getOverallHours(state);
 
-    let insertedEntry3 = {
-      hours: 4.3
-    };
-    let insertedEntries = [];
-    insertedEntries.push(insertedEntry);
-    insertedEntries.push(insertedEntry2);
-    insertedEntries.push(insertedEntry3);
-
-    const state = { insertedEntries, overallHours: 0 };
-    state.insertedEntries = insertedEntries;
-    mutations.RECALCULATE_HOURS(state);
-    expect(state.overallHours).toBe(12.55);
+    // expect
+    expect(overallHours).toBe(17.5);
   });
-  it('should add ADD_TIME_ENTRY_OBJECT into store', () => {
-    let payload = {
-      id: 0,
-      date: new Date(),
-      dateString: '2020-02-01',
-      project: 'Dermalog_A002',
-      description: 'Added store test',
-      hours: parseFloat('15'),
-      startTime: '10:00',
-      endTime: '15:00',
-      breakTime: '00:30'
-    };
+  it("should getInsertedTimesheetDates", () => {
+    // init
 
-    let state = {
-      isEditMode: false,
-      insertedTimeEntries: [],
-      insertedEntries: [],
-      overallHours: 0.0,
-      currentTimeEntry: {
-        id: 0,
-        date: new Date(),
-        project: '',
-        description: '',
-        hours: 0,
-        startTime: '',
-        endTime: '',
-        breakTime: '00:30'
-      },
-      pdfSettings: {
-        customerData: '',
-        ownData: {
-          email: '',
-          webpage: '',
-          name: '',
-          handyNumber: '',
-          address: ''
+    let firstDate = moment().toDate();
+    let secondDate = moment()
+      .add(1, "week")
+      .toDate();
+    let thirdDate = moment()
+      .add(2, "days")
+      .toDate();
+
+    const state = {
+      insertedEntries: [
+        {
+          date: firstDate
+        },
+        {
+          date: secondDate
+        },
+        {
+          date: thirdDate
         }
-      }
+      ]
     };
 
-    let currentTimeEntryMock = (mutations.SET_CURRENT_TIME_ENTRY = jest.fn());
-    let recalcHoursEntryMock = (mutations.RECALCULATE_HOURS = jest.fn());
+    // call method
+    let insertedTimesheetDates = getters.getInsertedTimesheetDates(state);
 
-    const store = new Vuex.Store({
-      state,
-      mutations
-    });
-
-    store.commit('ADD_TIME_ENTRY_OBJECT', state, payload);
-    store.commit('ADD_TIME_ENTRY_OBJECT', state, payload);
-
-    expect(state.insertedEntries.length).toEqual(2);
-    expect(currentTimeEntryMock).toBeCalledTimes(2);
-    expect(recalcHoursEntryMock).toBeCalledTimes(2);
+    // expect
+    expect(insertedTimesheetDates[0]).toBe(firstDate);
+    expect(insertedTimesheetDates[1]).toBe(secondDate);
+    expect(insertedTimesheetDates[2]).toBe(thirdDate);
   });
 });
